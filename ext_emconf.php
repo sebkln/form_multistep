026f420a57d6f204fa332d4aba9065280e63f989
Sebastian Klein <laitnin@gmx.net>
2025-05-13 20:45:45 +0200

    [TASK] Update TYPO3 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'form' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-13.4.99',
+            'form' => '11.5.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
